1338b5ec54adc227b338c65d78a9f82cd7db3131
vierageorge <joviera@gmail.com>
2020-10-05 14:34:18 -0500

    Extracting values from response with Jayway JsonPath, part ii

--- src/test/java/com/bestbuy/tests/JsonPathJaywayExamples.java
+++ src/test/java/com/bestbuy/tests/JsonPathJaywayExamples.java
@@ -2,6 +2,8 @@ package com.bestbuy.tests;
 
 import static io.restassured.RestAssured.*;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.AfterEach;
@@ -39,6 +41,7 @@ public class JsonPathJaywayExamples {
 		System.out.println("    ");
 	}
 	
+	@Disabled
 	@DisplayName("Get the root element")
 	@Test
 	public void getRoot() {
@@ -47,6 +50,7 @@ public class JsonPathJaywayExamples {
 	}
 	
 	
+	@Disabled
 	@DisplayName("Get the total value from the response")
 	@Test
 	public void getTotalFromResponse() {
@@ -54,4 +58,51 @@ public class JsonPathJaywayExamples {
 		System.out.println(String.valueOf(totalValue));
 	}
 	
+	@Disabled
+	@DisplayName("Get all the data elements")
+	@Test
+	public void getAllDataElements() {
+		List<HashMap<String, Object>> dataElements = JsonPath.read(jsonResponse, "$.data");
+		dataElements.stream()
+			.forEach(System.out::println);
+	}
+	
+	@Disabled
+	@DisplayName("Get first data element")
+	@Test
+	public void getFirtDataElement() {
+		HashMap<String, Object> firstElement = JsonPath.read(jsonResponse, "$.data[0]");
+		System.out.println(firstElement.toString());
+	}
+	
+	@Disabled
+	@DisplayName("Get the last data element")
+	@Test
+	public void getLastDataElement() {
+		HashMap<String, Object> lastDataElement = JsonPath.read(jsonResponse, "$.data[-1]");
+		System.out.println(lastDataElement.toString());
+	}
+	
+	@Disabled
+	@DisplayName("Get all the ids of the data elements")
+	@Test
+	public void getAllIdsUnderData() {
+		List<String> ids = JsonPath.read(jsonResponse, "$.data[*].id");
+		System.out.println(ids.toString());
+	}
+	
+	@Disabled
+	@DisplayName("Performs a deep scan of all the ids present in the response")
+	@Test
+	public void getAllIds() {
+		List<String> ids = JsonPath.read(jsonResponse, "$..[*].id");
+		System.out.println(ids.toString());
+	}
+	
+	@DisplayName("Get the price of the products whose price is less than 5")
+	@Test
+	public void getNameOfProductsWithPriceLessThan5() {
+		List<String> productNames = JsonPath.read(jsonResponse, "$.data[?(@.price < 5)].name");
+		productNames.stream().forEach(System.out::println);
+	}
 }
